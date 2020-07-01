79b28bb4591fa1bf58738dfe2f4d84a8fb6b73b1
Mohammad-537 <66563296+Mohammad-537@users.noreply.github.com>
2020-07-01 17:52:33 +0300

    سهل

---
+++ CLASSWORK6/app/src/main/java/com/example/classwork6/Movie.java
@@ -0,0 +1,10 @@
+package com.example.classwork6;
+
+public class Movie {
+    private String title;
+    private String mainActor;
+    private Double movieRate;
+    private int pgRate;
+    private String genre;
+
+}
